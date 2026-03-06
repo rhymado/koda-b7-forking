26a942825dbbb9ac55dc7fffb3b2e83cec0daae2
Muhammad Naufal Fadhilah <117992655+nopalllfd@users.noreply.github.com>
2026-03-06 15:24:42 +0700

    style: replace single-quote to double-quote

--- menu.js
+++ menu.js
@@ -1,45 +1,44 @@
-import { createInterface } from 'node:readline/promises';
+import { createInterface } from "node:readline/promises";
 
-import sayHelloWorld from './features/1_greeting.js';
-import toLowerCase from './features/9_to_lower_case.js';
+import sayHelloWorld from "./features/1_greeting.js";
+import toLowerCase from "./features/9_to_lower_case.js";
 
 async function menu() {
-  let exit = false;
-  const rl = createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  do {
-    console.log('======================');
-    console.log('Selamat Datang di Menu');
-    console.log('Silahkan pilih dari dibawah ini:');
-    console.log('1. Hello World');
-    console.log('9. To Lower Case');
-    console.log('0. Exit');
+    let exit = false
+    const rl = createInterface({
+        input: process.stdin,
+        output: process.stdout
+    })
+    do {
+        console.log("======================")
+        console.log("Selamat Datang di Menu")
+        console.log("Silahkan pilih dari dibawah ini:")
+        console.log("1. Hello World")
+        console.log("0. Exit")
 
-    try {
-      const input = await rl.question('Masukkan pilihan anda: ');
-      if (input === '0') {
-        exit = true;
-        console.log('🙏 Selamat tinggal 🙏');
-        rl.close();
-        return;
-      }
-      switch (input) {
-        case '1':
-          sayHelloWorld();
-          break;
-        case '9':
-          toLowerCase();
-          break;
-        default:
-          console.log('pilihan anda masih dalam tahap perkembangan\n');
-          break;
-      }
-    } catch (err) {
-      throw err;
-    }
-  } while (!exit);
+        try {
+            const input = await rl.question("Masukkan pilihan anda: ")
+            if (input === '0') {
+                exit = true
+                console.log("🙏 Selamat tinggal 🙏")
+                rl.close()
+                return
+            }
+            switch (input) {
+                case "1":
+                    sayHelloWorld()
+                    break;
+                case "9":
+                    toLowerCase()
+                    break;
+                default:
+                    console.log("pilihan anda masih dalam tahap perkembangan\n")
+                    break;
+            }
+        } catch (err) {
+            throw err
+        }
+    } while (!exit)
 }
 
-menu();
+menu()
